95a3b3276e5367237cea9b1456cd39db04e3045c
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-01-01 18:41:38 +0100

    add structure for events

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -2,7 +2,14 @@ import { DomListener } from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
   // возвращает шаблон компонента
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+  }
   toHTML() {
     return ''
+  }
+
+  init() {
+    this.initDOMListeners()
   }
 }
