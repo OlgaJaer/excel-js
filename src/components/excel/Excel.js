4fbd462209c4a1d0d0dd3331031f3c99846351ae
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-01-01 17:47:53 +0100

    create class Dom

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,21 +1,17 @@
 import { $ } from '@core/dom'
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+    this.$el = $(selector)
     this.components = options.components || []
   }
 
   getRoot() {
     const $root = $.create('div', 'excel')
-    // const $root = document.createElement('div')
-    // $root.classList.add('excel')
 
     this.components.forEach((Component) => {
       const $el = $.create('div', Component.className)
-      // const $el = document.createElement('div')
-      // $el.classList.add(Component.className)
       const component = new Component($el)
-      $el.innerHTML = component.toHTML()
+      $el.html(component.toHTML())
       $root.append($el)
     })
     return $root
